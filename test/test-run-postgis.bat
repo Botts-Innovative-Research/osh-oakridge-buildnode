74f90fb49d02080f3b771556b7312a8caa5d0710
earocorn <amavb789@gmail.com>
2025-09-23 16:45:48 -0500

    Fix postgis script

--- test/test-run-postgis.bat
+++ test/test-run-postgis.bat
@@ -1,19 +1,19 @@
 @echo off
-docker run -d --name postgres ^
+docker run -d --name test-postgis ^
   -e POSTGRES_DB=gis ^
   -e POSTGRES_USER=postgres ^
   -e POSTGRES_PASSWORD=postgres ^
   -p 5432:5432 ^
-  my-postgis:latest
+  oscar-postgis:latest
 
 :waitloop
-docker exec postgres pg_isready -U postgres | findstr /C:"accepting connections" >nul
+docker exec test-postgis pg_isready -U postgres | findstr /C:"accepting connections" >nul
 if %errorlevel%==0 (
   echo Postgres is ready!
   goto continue
 ) else (
   echo Waiting for Postgres...
-  timeout /t 2 >nul
+  timeout /t 5 >nul
   goto waitloop
 )
 
