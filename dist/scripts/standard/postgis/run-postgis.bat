54ac84c04c2c579adbf4f26640a1eec8d99c26b9
earocorn <amavb789@gmail.com>
2025-09-23 14:56:51 -0500

    Update runner to use secrets, add postgis script for windows, update oakridge-modules submodule

---
+++ dist/scripts/standard/postgis/run-postgis.bat
@@ -0,0 +1,9 @@
+@echo off
+docker build . --tag=oscar-postgis
+
+docker run ^
+  -e POSTGRES_DB=gis ^
+  -e POSTGRES_USER=postgres ^
+  -e POSTGRES_PASSWORD=postgres ^
+  -p 5432:5432 ^
+  oscar-postgis
