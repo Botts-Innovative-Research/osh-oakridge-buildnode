fc768b774b1208f32be54ba37ca0aad843608971
Alex Almanza <amavb789@gmail.com>
2025-11-03 21:28:04 -0600

    Add latest video fixes, PostGIS fixes, and script changes

--- dist/scripts/standard/postgis/run-postgis.sh
+++ dist/scripts/standard/postgis/run-postgis.sh
@@ -1,2 +1,2 @@
 sudo docker build . --tag=oscar-postgis
-docker run -e POSTGRES_DB=gis -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 oscar-postgis
+docker run -e PG_MAX_CONNECTIONS=500 -e POSTGRES_DB=gis -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 oscar-postgis
