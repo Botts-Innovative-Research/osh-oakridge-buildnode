8e64473b1fce12c8d1a3fc2df7567e25d6dee20f
earocorn <amavb789@gmail.com>
2025-11-17 14:25:06 -0600

    Update test config / module. Update commits for release 4

--- tools/sensorhub-test/src/main/java/org/sensorhub/node/test/TestSensorHub.java
+++ tools/sensorhub-test/src/main/java/org/sensorhub/node/test/TestSensorHub.java
@@ -4,7 +4,10 @@
  ******************************* END LICENSE BLOCK ***************************/
 package org.sensorhub.node.test;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import org.sensorhub.impl.SensorHub;
+import org.slf4j.LoggerFactory;
 
 
 public class TestSensorHub
@@ -15,6 +18,8 @@ public class TestSensorHub
     
     public static void main(String[] args) throws Exception
     {
+        Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        logger.setLevel(Level.DEBUG);
         SensorHub.main(new String[] {"tools/sensorhub-test/src/main/resources/config.json", "storage"});
     }
 }
